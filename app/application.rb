60af7a7a0b87f8e190a0bcba32ad30acf4f4a18e
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-02 21:55:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,6 +31,7 @@ class Application
 
     if @@items.include?(add_item)
       resp.write "added #{add_item}"
+      @@cart << add_item
     else
       resp.write "We don't have that item"
     end
